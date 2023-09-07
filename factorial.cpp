d61d069988f7f778d83ce74552b68352daf4a0e3
Ejder Aysun <ejderaysunn@gmail.com>
2023-09-07 06:16:19 +0300

    added recursive factorial function

--- factorial.cpp
+++ factorial.cpp
@@ -2,6 +2,12 @@
 typedef unsigned long long int ullint;
 
 // unsigned long long int = ullint
+ullint recursive_factorial(ullint n) {
+    if(n == 0) return 1;
+    else return n * recursive_factorial(n-1);
+}
+
+// unsigned long long int = ullint
 ullint iterative_factorial(ullint n) {
     ullint r = 1;
     for (ullint i = 2; i <= n; i++) {
